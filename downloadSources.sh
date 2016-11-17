30dc4606057feb7e3229f33f8768790f2b096eea
Jordan Christiansen <xordspar0@gmail.com>
2016-11-17 13:07:49 -0600

    Made the download script name downloaded files properly

--- downloadSources.sh
+++ downloadSources.sh
@@ -5,13 +5,13 @@ urls=('ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz'\
     'ftp://ftp.fu-berlin.de/pub/misc/movies/database/language.list.gz'\
     'ftp://ftp.fu-berlin.de/pub/misc/movies/database/running-times.list.gz'\
     'ftp://ftp.fu-berlin.de/pub/misc/movies/database/ratings.list.gz'\
-    'ftp://ftp.fu-berlin.de/pub/misc/movies/database/directors.list.gz'\
-    'ftp://ftp.fu-berlin.de/pub/misc/movies/database/writers.list.gz')
+    'ftp://ftp.fu-berlin.de/pub/misc/movies/database/directors.list.gz')
 
 if [ ! -d data ]; then
     mkdir data
 fi
 
-for url in $urls; do
-    curl $url | gunzip > "data/${url##ftp://*/}"
+for url in ${urls[@]}; do
+    file=$(echo $url | grep -Eo '[a-z-]+\.list')
+    curl $url | gunzip > "data/$file"
 done
