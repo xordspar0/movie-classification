9a4b927c1a208b177b8331da1064b96d43fbb2e8
Jordan Christiansen <xordspar0@gmail.com>
2016-11-18 17:54:15 -0600

    Collect data for every field, including the director

--- dataCombine.py
+++ dataCombine.py
@@ -28,14 +28,7 @@ res = {
 }
 
 with open(movieFile, encoding='iso-8859-1') as movieList:
-    i = 0 # for debugging
     for line in movieList:
-        # debug
-        i += 1
-        if i == 500:
-            break
-        # end debug
-
         title = ''
         year = ''
         genre = ''
@@ -68,11 +61,16 @@ with open(movieFile, encoding='iso-8859-1') as movieList:
                 'rating' : '',
             })
 
+        # # End early, for debugging.
+        # if len(data) >= 10000:
+        #     break
+        #
 print('Collected ' + str(len(data)) + ' movies.', file=sys.stderr)
 
 fields = ['country', 'language', 'time', 'rating']
 for i in range(0, len(fields)):
     with open(dataFiles[i], encoding='iso-8859-1') as dataFile:
+        print('Collecting ' + fields[i] + '...', file=sys.stderr)
         lastTitle = ''
         for movie in data:
             # Read through the file until we find the data for the current movie.
@@ -101,10 +99,37 @@ for i in range(0, len(fields)):
                         dataFile.seek(startPosition)
                         break
 
-        print('Finished collecting ' + fields[i] + '...', file=sys.stderr)
+        print('\tdone.', file=sys.stderr)
 
 # Find the director for each movie.
+directorRE = re.compile(r'(?P<director>[\S ]+)\t+(?P<title>[\S ]+) \(.+\)')
+movieRE = re.compile(r'\t\t\t(?P<title>.*) \(\S+\)')
+print('Collecting director...', file=sys.stderr)
+with open(dataFiles[4], encoding='iso-8859-1') as dataFile:
+    currentDirector = ''
+    for line in dataFile:
+        directorMatch = directorRE.match(line)
+        foundTitle = ''
+        if directorMatch:
+            currentDirector = directorMatch.group('director')
+            foundTitle = directorMatch.group('title')
+        else:
+            movieMatch = movieRE.match(line)
+            if movieMatch:
+                foundTitle = movieMatch.group('title')
 
+        if foundTitle:
+            try:
+                movie = next((movie for movie in data if movie['title'] == foundTitle))
+                movie['director'] = currentDirector
+            except StopIteration:
+                pass
+print('\tdone.', file=sys.stderr)
+
+# Remove any movies with missing data.
+for i in range(len(data)-1, -1, -1):
+    if '' in data[i].values():
+        data.pop(i)
 
 #################################
 # Write the data out to a file. #
@@ -112,7 +137,7 @@ for i in range(0, len(fields)):
 
 print(file=sys.stderr)
 
-headerString = 'Title\tYear\tCountry\tLanguage\tRunning time\tRating\tGenre'
+headerString = 'Title\tYear\tCountry\tLanguage\tDirector\tRunning time\tRating\tGenre'
 print(headerString)
 for movie in data:
     outputString = '\t'.join([
@@ -120,7 +145,7 @@ for movie in data:
         movie['year'],
         movie['country'],
         movie['language'],
-        # movie['director'],
+        movie['director'],
         str(movie['time']),
         str(movie['rating']),
         movie['genre']
