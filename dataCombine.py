6641b606cea182346d506e8c1624f6deebbc59a8
Jordan Christiansen <xordspar0@gmail.com>
2016-11-19 16:32:48 -0600

    Added the ability to specify an input file for dataCombine.py

--- dataCombine.py
+++ dataCombine.py
@@ -12,6 +12,8 @@ dataFiles = [
         'data/directors.list']
 
 workingDir = os.path.dirname(os.path.realpath(__file__)) + '/'
+if len(sys.argv) in [2, 4]:
+    movieFile = sys.argv.pop(2)
 if len(sys.argv) == 3:
     start = int(sys.argv[1])
     n = int(sys.argv[2])
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ./downloadSources.sh
+split -d -n l/64 data/genres.list data/genres.list.
 
-for i in {1..64}; do
-	python dataCombine.py $(($i * 10000)) 10000 > results/movies.tsv.$i &
+for i in {00..63}; do
+	echo "python dataCombine.py data/genres.list."$i" > results/movies.tsv.$i &"
 done
